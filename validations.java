9907b5e10229164deb3faca0a4567065fb97844d
Tasneem <tasneem23122004@gmail.com>
2025-06-16 17:33:43 +0530

    Update validations.java

--- validations.java
+++ validations.java
@@ -83,9 +83,3 @@ public class AdvancedValidationCar {
     // Getters and setters omitted for brevity
 }
 
-class DynamicCar {
-    public void setField(String name, Object value) {
-        // Simulate dynamic field creation (reflection in real code)
-        // Not statically analyzable
-    }
-}
