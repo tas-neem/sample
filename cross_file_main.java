b7ec6c8d5a719b68bab02fd1f4557ff56a8f27ed
Tasneem <tasneem23122004@gmail.com>
2025-06-16 13:07:23 +0530

    Update cross_file_main.java

--- cross_file_main.java
+++ cross_file_main.java
@@ -1,5 +1,24 @@
-from cross_file_utils import validate_age
+public class CrossFileMain {
+    public static void main(String[] args) {
+        try {
+            Person person = new Person(25); 
+            System.out.println("Person created with age: " + person.getAge());
 
-class Person:
-    def __init__(self, age):
-        self.age = validate_age(age)
+            Person invalidPerson = new Person(-5);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
+
+class Person {
+    private int age;
+
+    public Person(int age) {
+        this.age = CrossFileUtils.validateAge(age);
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
