3249fb34360f36785e900d9f5c7af85dbdcb0e3b
Tasneem <tasneem23122004@gmail.com>
2025-06-16 17:31:34 +0530

    Delete js_dynamic.js

--- js_dynamic.js
+++
@@ -1,9 +0,0 @@
-class DynamicObj {
-    constructor() {
-        // Dynamic property assignment
-        this['field_' + Math.random()] = 42;
-    }
-    setField(name, value) {
-        this[name] = value;
-    }
-}
